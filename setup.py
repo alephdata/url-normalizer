ebdba016c55353dc596b42e205b688b3a2882fae
Friedrich Lindenberg <friedrich@pudo.org>
2019-03-04 21:32:29 +0100

    Bump version: 1.2.0 → 1.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="urlnormalizer",
-    version="1.2.0",
+    version="version='1.2.1'",
     author="Tarashish Mishra",
     author_email="sunu@sunu.in",
     description="Normalize URLs. Mostly useful for deduplicating HTTP URLs.",
